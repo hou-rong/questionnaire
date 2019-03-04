fdae6e1540ae0b7114b510e82063db1cbe7d866a
Nurzhan Nogerbek <nnogerbek@beeline.kz>
2019-03-04 14:50:17 +0600

    renaming

--- models/factor.go
+++ models/factor.go
@@ -4,3 +4,7 @@ type Factor struct {
 	FactorID   int    `gorm:"primary_key" json:"factor_id"`
 	FactorName string `gorm:"not null;unique" json:"factor_name"`
 }
+
+func (Factor) TableName() string {
+	return "factor"
+}
