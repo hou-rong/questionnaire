9fb365ca87b6d62a68707398c49e6a0716c0ef1f
Nurzhan Nogerbek <nnogerbek@beeline.kz>
2019-04-17 18:17:59 +0600

    refactoring

--- controllers/answer.go
+++ controllers/answer.go
@@ -27,19 +27,35 @@ var CreateMultipleAnswer = func(responseWriter http.ResponseWriter, request *htt
 
 	// Build SQL statement.
 	var sqlStatement strings.Builder
-	sqlStatement.WriteString("INSERT INTO ANSWERS (SURVEY_ID, EMPLOYEE, QUESTION_ID, QUESTION_TEXT, OPTION_ID, OPTION_TEXT) SELECT '")
-	sqlStatement.WriteString(requestBody.SurveyID)
-	sqlStatement.WriteString("' SURVEY_ID, '")
-	sqlStatement.WriteString(requestBody.Employee)
-	sqlStatement.WriteString("' EMPLOYEE, UNNEST(ARRAY[")
-	sqlStatement.WriteString(utils.ConvertIntArrayToString(requestBody.QuestionID))
-	sqlStatement.WriteString("]) AS QUESTION_ID, UNNEST(ARRAY[")
-	sqlStatement.WriteString(utils.ConvertStringArrayToString(requestBody.QuestionText))
-	sqlStatement.WriteString("]) AS QUESTION_TEXT, UNNEST(ARRAY[")
-	sqlStatement.WriteString(utils.ConvertIntArrayToString(requestBody.OptionID))
-	sqlStatement.WriteString("]) AS OPTION_ID, UNNEST(ARRAY[")
-	sqlStatement.WriteString(utils.ConvertStringArrayToString(requestBody.OptionText))
-	sqlStatement.WriteString("]) AS OPTION_TEXT")
+
+	// check the length of the value in the request body element.
+	if len(requestBody.OptionID) != 0 {
+		sqlStatement.WriteString("INSERT INTO ANSWERS (SURVEY_ID, EMPLOYEE, QUESTION_ID, QUESTION_TEXT, OPTION_ID, OPTION_TEXT) SELECT '")
+		sqlStatement.WriteString(requestBody.SurveyID)
+		sqlStatement.WriteString("' SURVEY_ID, '")
+		sqlStatement.WriteString(requestBody.Employee)
+		sqlStatement.WriteString("' EMPLOYEE, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertIntArrayToString(requestBody.QuestionID))
+		sqlStatement.WriteString("]) AS QUESTION_ID, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertStringArrayToString(requestBody.QuestionText))
+		sqlStatement.WriteString("]) AS QUESTION_TEXT, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertIntArrayToString(requestBody.OptionID))
+		sqlStatement.WriteString("]) AS OPTION_ID, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertStringArrayToString(requestBody.OptionText))
+		sqlStatement.WriteString("]) AS OPTION_TEXT")
+	} else {
+		sqlStatement.WriteString("INSERT INTO ANSWERS (SURVEY_ID, EMPLOYEE, QUESTION_ID, QUESTION_TEXT, OPTION_TEXT) SELECT '")
+		sqlStatement.WriteString(requestBody.SurveyID)
+		sqlStatement.WriteString("' SURVEY_ID, '")
+		sqlStatement.WriteString(requestBody.Employee)
+		sqlStatement.WriteString("' EMPLOYEE, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertIntArrayToString(requestBody.QuestionID))
+		sqlStatement.WriteString("]) AS QUESTION_ID, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertStringArrayToString(requestBody.QuestionText))
+		sqlStatement.WriteString("]) AS QUESTION_TEXT, UNNEST(ARRAY[")
+		sqlStatement.WriteString(utils.ConvertStringArrayToString(requestBody.OptionText))
+		sqlStatement.WriteString("]) AS OPTION_TEXT")
+	}
 
 	// Make SQL query by "database/sql" package.
 	_, err := database.DBSQL.Exec(sqlStatement.String()); if err != nil {
